0a17af7a85c39be2b8bd1952f41d2c3453e00343
Abdelsalam ElTamawy <abdelsalam@solom.dev>
2023-12-03 02:50:54 +0200

    day2 passed

--- 2023/day2/main.rs
+++ 2023/day2/main.rs
@@ -1,33 +1,170 @@
-use regex::Regex;
+use std::cmp::max;
+use std::iter::Sum;
+use std::ops::Add;
 
-const RED: i32 = 12;
-const GREEN: i32 = 13;
-const BLUE: i32 = 14;
-
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 struct Round {
-    red: i32,
-    green: i32,
-    blue: i32,
+    red: u32,
+    green: u32,
+    blue: u32,
 }
 
+impl Round {
+    fn power(&self) -> u32 {
+        self.red * self.green * self.blue
+    }
+}
+
+impl Add for Round {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            red: self.red + other.red,
+            green: self.green + other.green,
+            blue: self.blue + other.blue,
+        }
+    }
+}
+
+impl Sum for Round {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(
+            Self {
+                red: 0,
+                green: 0,
+                blue: 0,
+            },
+            |a, b| a + b,
+        )
+    }
+}
+
+// FIXME: borked
+impl std::cmp::PartialOrd for Round {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self.red <= other.red && self.green <= other.green && self.blue <= other.blue {
+            Some(std::cmp::Ordering::Equal)
+        } else if self.red < other.red || self.green < other.green || self.blue < other.blue {
+            Some(std::cmp::Ordering::Less)
+        } else {
+            Some(std::cmp::Ordering::Greater)
+        }
+    }
+}
+
+const BAG: Round = Round {
+    red: 12,
+    green: 13,
+    blue: 14,
+};
+
+#[derive(Debug)]
 struct Game {
-    id: i32,
+    id: u32,
     rounds: Vec<Round>,
 }
 
-fn part1(input: &str) -> i32 {
-    // (?m)^Game (?P<id>\d+): ((?:((?P<num>\d+) (?P<col>\w+))(?:, )?)+(:?; )?)+
-    let re = Regex::new(r"(?m)^Game (?P<id>\d+): ((,?.?(\d+ \w+))+;?)+$").unwrap();
-    let _ = re.captures_iter(input).for_each(|line| {
-        let id = line["id"].parse::<i32>().unwrap();
-        // println!("{:?}", id);
-        println!("{:?}", line.get(3));
-    });
-    todo!()
+fn part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut s = line.split(":");
+            let game_id = s.next().unwrap().split(" ").skip(1).next().unwrap();
+            let rounds = s
+                .next()
+                .unwrap()
+                .split(";")
+                .map(|round| {
+                    let mut r: u32 = 0;
+                    let mut g: u32 = 0;
+                    let mut b: u32 = 0;
+                    for (count, color) in round.split(",").map(|x| {
+                        let mut iter = x.split_whitespace();
+                        (iter.next().unwrap(), iter.next().unwrap())
+                    }) {
+                        let count = count.trim().parse().unwrap();
+                        match color.trim() {
+                            "red" => r = count,
+                            "green" => g = count,
+                            "blue" => b = count,
+                            _ => panic!("invalid color"),
+                        }
+                    }
+                    Round {
+                        red: r,
+                        green: g,
+                        blue: b,
+                    }
+                })
+                .collect::<Vec<_>>();
+            Game {
+                id: game_id.parse::<u32>().unwrap(),
+                rounds: rounds,
+            }
+        })
+        .filter(|game| {
+            game.rounds
+                .iter()
+                .map(|round| {
+                    round.red <= BAG.red && round.green <= BAG.green && round.blue <= BAG.blue
+                })
+                .all(|x| x)
+        })
+        .map(|game| game.id)
+        .sum()
 }
 
-fn part2(input: &str) -> i32 {
-    todo!()
+fn part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut s = line.split(":");
+            let game_id = s.next().unwrap().split(" ").skip(1).next().unwrap();
+            let rounds = s
+                .next()
+                .unwrap()
+                .split(";")
+                .map(|round| {
+                    let mut r: u32 = 0;
+                    let mut g: u32 = 0;
+                    let mut b: u32 = 0;
+                    for (count, color) in round.split(",").map(|x| {
+                        let mut iter = x.split_whitespace();
+                        (iter.next().unwrap(), iter.next().unwrap())
+                    }) {
+                        let count = count.trim().parse().unwrap();
+                        match color.trim() {
+                            "red" => r = count,
+                            "green" => g = count,
+                            "blue" => b = count,
+                            _ => panic!("invalid color"),
+                        }
+                    }
+                    Round {
+                        red: r,
+                        green: g,
+                        blue: b,
+                    }
+                })
+                .collect::<Vec<_>>();
+            Game {
+                id: game_id.parse::<u32>().unwrap(),
+                rounds: rounds,
+            }
+        })
+        .map(|game| {
+            game.rounds
+                .into_iter()
+                .reduce(|acc, e| Round {
+                    red: max(acc.red, e.red),
+                    green: max(acc.green, e.green),
+                    blue: max(acc.blue, e.blue),
+                })
+                .unwrap()
+                .power()
+        })
+        .sum()
 }
 
 fn main() {
