a5c7aa4c82539b311c14bc410000d91e39c2b796
Abdelsalam ElTamawy <abdelsalam@solom.dev>
2023-12-04 02:50:49 +0200

    day templates

---
+++ templates/python/main.py
@@ -0,0 +1,10 @@
+def part1(input: str) -> int:
+	0
+
+def part2(input: str) -> int:
+	0
+
+if __name__ == "__main__":
+	input = open("input.txt").read()
+	print("Part1:", part1(input))
+	print("Part2:", part1(input))
---
+++ templates/rust/main.rs
@@ -0,0 +1,12 @@
+fn part1(input: &str) -> u32 {
+	0
+}
+
+fn part2(input: &str) -> u32 {
+	0
+}
+
+fn main() {
+    let input = std::fs::read_to_string("input.txt").expect("Failed to read input.txt");
+    println!("Part1: {}\nPart2: {}", part1(&input), part2(&input));
+}
